cc853374ba470efcd34e9ac5444aa7faecdfa5d8
tocino155 <101911354+tocino155@users.noreply.github.com>
2022-06-16 18:24:10 -0500

    “Creado_el_proyecto_inicial”

---
+++ resources/views/auth/forgot-password.blade.php
@@ -0,0 +1,57 @@
+<x-guest-layout>
+    <x-jet-authentication-card>
+        <x-slot name="logo">
+            <x-jet-authentication-card-logo />
+        </x-slot>
+
+            <div class="container-fluid fixed-top p-4">
+        <div class="col-12">
+            <div class="d-flex justify-content-end">
+                @if (Route::has('login'))
+                    <div class="">
+                        @auth
+                            <a href="{{ url('/dashboard') }}" class="text-muted">Dashboard</a>
+                        @else
+                            <a href="{{ route('login') }}" class="text-muted">Log in</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="ms-4 text-muted">Register</a>
+                            @endif
+                        @endif
+                    </div>
+                @endif
+            </div>
+        </div>
+    </div>
+
+        <div class="card-body">
+
+            <div class="mb-3">
+                {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            </div>
+
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            <x-jet-validation-errors class="mb-3" />
+
+            <form method="POST" action="/forgot-password">
+                @csrf
+
+                <div class="mb-3">
+                    <x-jet-label value="Email" />
+                    <x-jet-input type="email" name="email" :value="old('email')" required autofocus />
+                </div>
+
+                <div class="d-flex justify-content-end mt-4">
+                    <x-jet-button>
+                        {{ __('Email Password Reset Link') }}
+                    </x-jet-button>
+                </div>
+            </form>
+        </div>
+    </x-jet-authentication-card>
+</x-guest-layout>
