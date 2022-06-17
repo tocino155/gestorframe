cec02672a781f9ab1ef542b5fd6de249d8d13e5d
Ferchoshaggy <fernandoshaggy562@gmail.com>
2022-06-16 20:44:59 -0500

    ventana

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,23 +11,18 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
         @livewireStyles
 
         <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="font-sans antialiased bg-light">
         <x-jet-banner />
         @livewire('navigation-menu')
 
-        <!-- Page Heading -->
-        <header class="d-flex py-3 bg-white shadow-sm border-bottom">
-            <div class="container">
-                {{ $header }}
-            </div>
-        </header>
+
 
         <!-- Page Content -->
         <main class="container my-5">
